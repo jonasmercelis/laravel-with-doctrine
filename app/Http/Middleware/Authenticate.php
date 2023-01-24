7205f90c909d824e5c9562f86463e103ddfe0492
jonasmercelis <jonasmercelis@icloud.com>
2023-01-24 22:53:48 +0100

    Improvements, inject dependencies

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -4,10 +4,20 @@ declare(strict_types=1);
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Contracts\Auth\Factory as AuthFactory;
+use Illuminate\Contracts\Routing\UrlGenerator;
 use Illuminate\Http\Request;
 
 final class Authenticate extends Middleware
 {
+    private readonly UrlGenerator $urlGenerator;
+
+    public function __construct(AuthFactory $authFactory, UrlGenerator $urlGenerator)
+    {
+        parent::__construct($authFactory);
+        $this->urlGenerator = $urlGenerator;
+    }
+
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
@@ -17,7 +27,8 @@ final class Authenticate extends Middleware
     protected function redirectTo($request): ?string
     {
         if (! $request->expectsJson()) {
-            return route('login');
+
+            return $this->urlGenerator->route('login');
         }
 
         return null;
--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -6,16 +6,20 @@ namespace App\Http\Middleware;
 use App\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Auth\AuthManager;
+use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
 use Symfony\Component\HttpFoundation\Response;
 
 final class RedirectIfAuthenticated
 {
     private readonly AuthManager $auth;
+    private readonly Redirector $redirector;
 
-    public function __construct(AuthManager $auth)
+    public function __construct(AuthManager $auth, Redirector $redirector)
     {
         $this->auth = $auth;
+        $this->redirector = $redirector;
     }
 
     /**
@@ -23,7 +27,7 @@ final class RedirectIfAuthenticated
      *
      * @param Request $request
      * @param Closure(Request): (Response) $next
-     * @param  string|null  ...$guards
+     * @param  Guard[]  ...$guards
      * @return Response
      */
     public function handle(Request $request, Closure $next, ...$guards): Response
@@ -32,7 +36,7 @@ final class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if ($this->auth->guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                return $this->redirector->to(RouteServiceProvider::HOME);
             }
         }
 
